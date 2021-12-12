df08f09fe1ec22c1499796fc3bf4f59ab76e1636
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-12 02:20:12 -0500

    lesson 3 completed

--- 03-jsx/src/App.js
+++ 03-jsx/src/App.js
@@ -1,31 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-
-    [
-      React.createElement("h1", { id: "my-brand" }, "Adopt Me?"),
-      React.createElement(Pet, {
-        name: "Luna??",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Dog",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Sudo",
-        animal: "Dog",
-        breed: "Wheaten Terrier",
-      }),
-    ]
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Jax" animal="Dog" breed="Corgi" />
+      <Pet name="Pickles" animal="Dog" breed="Chihuahua" />
+      <Pet name="Duke" animal="Dog" breed="Lab" />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
