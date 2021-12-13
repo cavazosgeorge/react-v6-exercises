bb4df768c587f9f338c0545e0cdcc38b09db252b
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-12 23:48:35 -0500

    implemented v6 routing

--- 07-react-router/src/App.js
+++ 07-react-router/src/App.js
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
