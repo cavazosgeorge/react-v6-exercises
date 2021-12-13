d0bd23c2ec565337fcd7e7a5e1838db9696a2b92
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-12 21:27:15 -0500

    created custom breed hook and imported to search params

--- 05-custom-hooks/src/SearchParams.js
+++ 05-custom-hooks/src/SearchParams.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptiller"];
 
@@ -8,7 +9,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -60,7 +61,7 @@ const SearchParams = () => {
           >
             <option />
             {breeds.map((breed) => (
-              <option value={breed} key={breed}>
+              <option key={breed} value={breed}>
                 {breed}
               </option>
             ))}
