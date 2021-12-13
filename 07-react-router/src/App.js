eb4a2d5e81ddcfb62e04ac5038b06160582762c7
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-13 00:05:28 -0500

    updated Adopt Me! tag to Link w/route

--- 07-react-router/src/App.js
+++ 07-react-router/src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -8,8 +8,12 @@ import Details from "./Details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <BrowserRouter>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
